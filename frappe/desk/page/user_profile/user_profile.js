dc02c19eb66103a31594a1c1599bd6beb2f0948b
Rinzin <rinzinn1996@gmail.com>
2024-06-24 12:05:50 +0600

    attendance

--- frappe/desk/page/user_profile/user_profile.js
+++ frappe/desk/page/user_profile/user_profile.js
@@ -1,6 +1,466 @@
 frappe.pages["user-profile"].on_page_load = function (wrapper) {
-	frappe.require("user_profile_controller.bundle.js", () => {
-		let user_profile = new frappe.ui.UserProfile(wrapper);
+
+	frappe.ui.make_app_page({
+		parent: wrapper,
+		title: __('User Profile'),
+	});
+
+	let user_profile = new UserProfile(wrapper);
+	$(wrapper).bind('show', ()=> {
 		user_profile.show();
 	});
 };
+
+class UserProfile {
+
+	constructor(wrapper) {
+		this.wrapper = $(wrapper);
+		this.page = wrapper.page;
+		this.sidebar = this.wrapper.find('.layout-side-section');
+		this.main_section = this.wrapper.find('.layout-main-section');
+	}
+
+	show() {
+		let route = frappe.get_route();
+		this.user_id = route[1] || frappe.session.user;
+
+		//validate if user
+		if (route.length > 1) {
+			frappe.db.exists('User', this.user_id).then( exists => {
+				if (exists) {
+					this.make_user_profile();
+				} else {
+					frappe.msgprint(__('User does not exist'));
+				}
+			});
+		} else {
+			this.user_id = frappe.session.user;
+			this.make_user_profile();
+		}
+	}
+
+	make_user_profile() {
+		frappe.set_route('user-profile', this.user_id);
+		this.user = frappe.user_info(this.user_id);
+		this.page.set_title(this.user.fullname);
+		this.setup_transaction_link();
+		this.main_section.empty().append(frappe.render_template('user_profile'));
+		this.render_user_details();
+		if(this.user_id != "Administrator"){
+		this.checkin_info();
+		}
+		this.setup_punching_button();
+	}
+	
+	setup_transaction_link() {
+		this.$user_search_button = this.page.set_secondary_action('<b>Transactions Link</b>', () => {
+			frappe.set_route('')
+		});
+	}
+
+	//---------------------This section is for To Do List-----------------------------
+	get_open_documents() {
+		this.open_docs_config = {
+			ToDo: { label: __('To Do') },
+			Event: { label: __('Calendar'), route: 'List/Event/Calendar' }
+		};
+
+		frappe.ui.notifications.get_notification_config().then(r => {
+			// this.open_document_list = r;
+
+			this.$to_do_list = this.wrapper.find('.to-do-list');
+
+			var item_list=`<table class="table table-bordered small" style="margin: 0px 0px 10px 0px;">
+			<thead style="background-color: #9badf052;">
+				<tr>
+					<th style="width: 20%" class="text-center">${__('Transaction')}</th>
+					<th style="width: 20%" class="text-center">${__('Count')}</th>
+				</tr>
+			</thead>`;
+			var open_docs = r.open_count_doctype;
+			var docstatus = 'docstatus=Draft'
+				
+			for (const key in open_docs){
+				if(open_docs[key] && key != 'Employee Checkin' && key != 'Employee PF' && key != 'Payroll Entry' && key != 'Salary Slip' && key != 'Direct Payment'){
+					item_list += `<tr>
+					<td><span class="indicator red"></span>
+					<a class = "link-content" href="#List/${key}?${docstatus}" target="_blank">${key}
+					</a></td>
+					<td class="text-center"><span class="badge">${open_docs[key]}</span></td>
+					</tr>`;
+				}
+			}
+			item_list += '</table>'
+			this.$to_do_list.html(item_list);
+		});
+	}
+	//-------------------------------End--------------------------------------------
+
+
+
+
+
+
+
+	get_year(date_str) {
+		return date_str.substring(0, date_str.indexOf('-'));
+	}
+
+	create_attendance_dashboard_filters() {
+		let filters = [
+			{
+				label: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",
+				"December"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+				options: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November",	"December"],
+				action: (selected_item) => {
+					this.employee_attendance_dashboard(selected_item);
+				}
+			},
+		];
+		this.render_chart_filters(filters, '.filters-container');
+	}
+	//New
+	create_training_dashboard_filters() {
+		let filters = [
+			{
+				label: this.get_year(frappe.datetime.now_date()),
+				options: this.get_years_since_creation(),
+				action: (selected_item) => {
+					this.employee_trainings(frappe.datetime.obj_to_str(selected_item));
+				}
+			},
+		];
+		this.render_chart_filters(filters, '.training_filters-container');
+	}
+
+	get_years_since_creation() {
+		//Get years since user account created
+		this.user_creation = frappe.boot.user.creation;
+		let creation_year = this.get_year(this.user_creation);
+		let current_year = this.get_year(frappe.datetime.now_date());
+		let years_list = [];
+		for (var year = current_year; year >= creation_year; year--) {
+			years_list.push(year);
+		}
+		return years_list;
+	}
+
+
+	render_chart_filters(filters, container, append) {
+		filters.forEach(filter => {
+			let chart_filter_html = `<div class="chart-filter pull-right">
+				<a class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<button class="btn btn-default btn-xs">
+						<span class="filter-label">${filter.label}</span>
+						<span class="caret"></span>
+					</button>
+				</a>`;
+			let options_html;
+
+			if (filter.fieldnames) {
+				options_html = filter.options.map((option, i) =>
+					`<li><a data-fieldname = "${filter.fieldnames[i]}">${option}</a></li>`).join('');
+			} else {
+				options_html = filter.options.map( option => `<li><a>${option}</a></li>`).join('');
+			}
+
+			let dropdown_html = chart_filter_html + `<ul class="dropdown-menu">${options_html}</ul></div>`;
+			let $chart_filter = $(dropdown_html);
+
+			if (append) {
+				$chart_filter.prependTo(this.wrapper.find(container));
+			} else $chart_filter.appendTo(this.wrapper.find(container));
+
+			$chart_filter.find('.dropdown-menu').on('click', 'li a', (e) => {
+				let $el = $(e.currentTarget);
+				let fieldname;
+				if ($el.attr('data-fieldname')) {
+					fieldname = $el.attr('data-fieldname');
+				}
+				let selected_item = $el.text();
+				$el.parents('.chart-filter').find('.filter-label').text(selected_item);
+				filter.action(selected_item, fieldname);
+			});
+		});
+
+	}
+
+	//---------------------Inserting Employee Checkin----------------------------
+	make_employee_checkin(checkin_type, half_day, half_day_leave){
+		var ct = ""
+		if(checkin_type == "Office IN" && half_day == 0 && half_day_leave == 0){
+			ct = "Office OUT"
+		}
+		else if((half_day == 1 || half_day_leave == 1) &&  checkin_type == "Office IN"){
+			ct = "Office OUT"
+		}
+		else{
+			ct = "Office IN"
+		}
+		frappe.call({
+			method:"frappe.desk.page.user_profile.user_profile.get_employee_info",
+			args: ({"user":frappe.session.user, "checkin_type":ct, "half_day": half_day}),
+			callback: function(r){
+				if(r.message){
+					console.log(r.message)
+					if((r.message[0].flag == 1 && ct == "Office IN") || (r.message[0].oo_flag == 1 && ct == "Office OUT")){
+						let reason_dialog = new frappe.ui.Dialog({
+							title: __('Late coming/Early Exit Reason'),
+							fields: [
+								{
+									fieldtype: 'Small Text',
+									fieldname: 'reason',
+									label: 'Reason',
+									reqd: 1,
+								}
+							],
+							primary_action: values => {
+								reason_dialog.disable_primary_action();
+								frappe.xcall('frappe.desk.page.user_profile.user_profile.make_employee_checkin', {
+									"employee": r.message[0].employee,
+									"employee_name": r.message[0].employee_name,
+									"shift_type": r.message[0].shift_type,
+									"time": r.message[0].time,
+									"time_difference": r.message[0].time_difference,
+									reason: values['reason'],
+									checkin_type: ct
+								}).then(user => {
+									reason_dialog.hide();
+								}).finally(() => {
+									reason_dialog.enable_primary_action();
+								});
+								let alert_dialog = new frappe.ui.Dialog({
+									title: 'Your Record is updated successfully',
+									primary_action: values => {
+										alert_dialog.disable_primary_action();
+										window.location.reload()
+									},
+									primary_action_label: 'OK'
+								});
+								alert_dialog.show();
+							},
+							primary_action_label: __('Save')
+						});
+						reason_dialog.show();
+					}
+					else{
+						frappe.confirm(
+							__('Are you sure?'),
+							function(){
+								frappe.call({
+									method: "frappe.desk.page.user_profile.user_profile.make_employee_checkin",
+									args: {
+											"employee": r.message[0].employee,
+											"employee_name": r.message[0].employee_name,
+											"shift_type": r.message[0].shift_type,
+											"time": r.message[0].time,
+											"time_difference": r.message[0].time_difference,
+											checkin_type: ct
+										},
+									callback: function(r){
+										// msgprint('Your Record is updated successfully');
+										window.location.reload();
+									}
+								});	
+							},
+							function(){
+								// frappe.show_alert('NO');
+							}
+						)
+					}
+				}
+			}
+		})
+	}
+	//-----------------------------End-----------------------------------
+
+	edit_profile() {
+		let edit_profile_dialog = new frappe.ui.Dialog({
+			title: __('Edit Profile'),
+			fields: [
+				{
+					fieldtype: 'Attach Image',
+					fieldname: 'user_image',
+					label: 'Profile Image',
+				},
+				{
+					fieldtype: 'Data',
+					fieldname: 'interest',
+					label: 'Interests',
+				},
+				{
+					fieldtype: 'Column Break'
+				},
+				{
+					fieldtype: 'Data',
+					fieldname: 'location',
+					label: 'Location',
+				},
+				{
+					fieldtype: 'Section Break',
+					fieldname: 'Interest',
+				},
+				{
+					fieldtype: 'Small Text',
+					fieldname: 'bio',
+					label: 'Bio',
+				}
+			],
+			primary_action: values => {
+				edit_profile_dialog.disable_primary_action();
+				frappe.xcall('frappe.desk.page.user_profile.user_profile.update_profile_info', {
+					profile_info: values
+				}).then(user => {
+					user.image = user.user_image;
+					this.user = Object.assign(values, user);
+					edit_profile_dialog.hide();
+					this.render_user_details();
+				}).finally(() => {
+					edit_profile_dialog.enable_primary_action();
+				});
+			},
+			primary_action_label: __('Save')
+		});
+
+		edit_profile_dialog.set_values({
+			user_image: this.user.image,
+			location: this.user.location,
+			// user_sign: this.user.user_sign,
+			interest: this.user.interest,
+			// scan_sign: this.user.scan_sign,
+			bio: this.user.bio
+		});
+		edit_profile_dialog.show();
+	}
+
+	render_user_details() {
+		this.sidebar.empty().append(frappe.render_template('user_profile_sidebar', {
+			user_image: this.user.image,
+			user_abbr: this.user.abbr,
+			user_location: this.user.location,
+			user_interest: this.user.interest,
+			user_bio: this.user.bio,
+		}));
+
+
+		this.setup_user_profile_links();
+	}
+
+	// Sidebar Links
+	setup_user_profile_links() {
+		if (this.user_id !== frappe.session.user) {
+			this.wrapper.find('.profile-links').hide();
+		} else {
+			this.wrapper.find('.edit-profile-link').on('click', () => {
+				this.edit_profile();
+			});
+
+			this.wrapper.find('.transaction-link').on('click', () => {
+				this.go_to_desk();
+			});
+
+		}
+	}
+
+	go_to_desk() {
+		// frappe.set_route('Form', 'User', this.user_id);
+		frappe.set_route('');
+	}
+
+	// Enabling and disabling employee checkin button
+	setup_punching_button(){
+		var checkin_type = ""
+		var half_day = 0
+		var holiday = 0
+		var half_day_leave = 0
+		frappe.call({
+			method: "frappe.desk.page.user_profile.user_profile.get_employee_checkin_info",
+			async: false,
+			callback: function(r){
+				checkin_type = r.message[0]
+				half_day = r.message[1];
+				holiday = r.message[2];
+				half_day_leave = r.message[3];
+			}
+		});
+
+		if(frappe.session.user == 'Administrator'){
+			this.wrapper.find('.office-in-button').hide();
+			this.wrapper.find('.office-out-button').hide();
+		}
+		else if(checkin_type == "Office IN" && half_day == 0 && holiday == 0 && half_day_leave == 0){
+			this.wrapper.find('.office-in-button').hide();
+			this.wrapper.find('.office-out').on('click', () => {
+				this.make_employee_checkin(checkin_type, half_day, half_day_leave);
+			});
+		}
+		else if(checkin_type == "Office IN" && holiday == 0){					
+			this.wrapper.find('.office-in-button').hide();
+			this.wrapper.find('.office-out').on('click', () => {
+				this.make_employee_checkin(checkin_type, half_day, half_day_leave);
+			});
+		}
+		else if(checkin_type == "Office OUT"){
+			this.wrapper.find('.office-in-button').hide();
+			this.wrapper.find('.office-out-button').hide();
+		}
+		else if ((half_day==1 || half_day_leave == 1) && checkin_type == "Office IN" && holiday == 0){
+			this.wrapper.find('.office-in-button').hide();
+			this.wrapper.find('.office-out').on('click', () => {
+				this.make_employee_checkin(checkin_type, half_day, half_day_leave);
+			});
+		}
+		else if(holiday == 1){
+			this.wrapper.find('.office-in-button').hide();
+			this.wrapper.find('.office-out-button').hide();
+		}
+		else{
+			this.wrapper.find('.office-out-button').hide();
+			this.wrapper.find('.office-in').on('click', () => {
+				this.make_employee_checkin(checkin_type, half_day, half_day_leave);
+			});
+		}
+	}
+
+	get_display_time()	{
+		return frappe.xcall("frappe.desk.page.user_profile.user_profile.get_display_time", {
+			async: false,
+		}).then(r =>{
+			this.d_time = r[0].d_time;
+		});
+	}
+
+	display_time() {
+		let $display_t = this.wrapper.find('.display-time');
+
+		this.get_display_time().then(() => {
+			let html = $(__(`<span>${this.d_time}</span>`,[this.d_time]));
+			setInterval(()=>this.display_time(),1000)
+				
+			$display_t.html(html);
+		});
+	}
+
+	get_checkin_info() {
+		return frappe.xcall('frappe.desk.page.user_profile.user_profile.get_checkin_info', {
+			user: this.user_id,
+		}).then(r => {
+			this.office_in = r[0].office_in;
+			this.office_out = r[0].office_out;
+			this.date = r[0].date;
+		});
+	}
+
+	checkin_info() {
+		let $checkin_details = this.wrapper.find('.checkin_details');
+
+		this.get_checkin_info().then(() => {
+				let html = $(__(`<p style="color:#1f1e1e; font-size:16px; "><b>${__('Date: ')}</b><span class="rank"><b>${this.date}</b></span></p>
+					<p style="color:#15F906; font-size:14px;"><b>${__('Office In: ')}</b><span class="rank"><b>${this.office_in}</b></span></p>
+					<p style="color:#f44336; font-size:14px;"><b>${__('Office Out: ')}</b><span class="rank"><b>${this.office_out}</b></span></p>
+				`, [this.date, this.office_in,this.office_out]));
+
+				$checkin_details.append(html);
+		});
+	}
+}
--- frappe/desk/page/user_profile/user_profile.py
+++ frappe/desk/page/user_profile/user_profile.py
@@ -3,8 +3,136 @@ from datetime import datetime
 import frappe
 from frappe.query_builder import Interval, Order
 from frappe.query_builder.functions import Date, Sum, UnixTimestamp
-from frappe.utils import getdate
+from frappe.utils import getdate, nowdate, cint, flt
+import calendar
+from erpnext.setup.doctype.employee.employee import get_holiday_list_for_employee
 
+@frappe.whitelist()
+def get_employee_info(user=None, checkin_type=None, half_day=None):
+	if not user: user = frappe.session.user
+	FMT = '%H:%M:%S'
+	data = []
+	flag = 0
+	office_out_flag = 0
+	date = datetime.strptime(str(nowdate()), "%Y-%m-%d")
+	t = datetime.now().time().strftime(FMT)
+	time = datetime.strptime(str(t),FMT)
+	emp = frappe.db.sql("""
+				select e.name as employee, e.employee_name, e.department,
+    			ifnull((select b.shift_type from `tabAssign Shift` b, `tabShift Details` sd, `tabShift Type` st where sd.parent = b.name and sd.employee = e.employee and b.shift_type = st.name and MONTHNAME('{1}') = b.month and YEAR('{1}') = b.fiscal_year and sd.{2} = 1 and b.docstatus = 1),e.default_shift) as shift_type,
+				e.division, e.section, e.designation
+				from `tabEmployee` e
+				where e.user_id = "{3}"
+	""".format(time, date, str(date.day), user), as_dict=True)
+	# frappe.throw(str(emp))
+ 
+	leave = frappe.db.sql("""
+		select name from `tabLeave Application` where half_day = 1 and employee = '{0}' and '{1}' between from_date and to_date and docstatus = 1
+					""".format(emp[0].employee, date), as_dict=True)
+	# frappe.throw(str(leave))
+
+	att_request = frappe.db.sql("""
+		select name from `tabAttendance Request` where employee = '{0}'
+		and '{1}' between from_date and to_date and half_day = 1
+    """.format(emp[0].employee, date.date()), as_dict=1)
+	# frappe.throw(str(att_request))
+
+	if leave and leave[0].name != None and leave[0].name != '' and emp[0].shift_type == "General Shift":
+		leave_type = frappe.db.get_value("Leave Application",leave[0].name,"half_day_type")
+		if leave_type == "Second Half":
+			actual_start_time = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "start_time")), FMT)
+			actual_start_time = actual_start_time.time()
+			actual_et = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "first_half_time")),FMT)
+			actual_end_time = actual_et.time()
+		elif leave_type == "First Half":
+			actual_st = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "second_half_time")),FMT)
+			actual_start_time = actual_st.time()
+			actual_end_time = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "end_time")),FMT)
+			actual_end_time = actual_end_time.time()
+	elif att_request and att_request[0].name != None and att_request[0].name != '':
+		half_day_type = frappe.db.get_value("Attendance Request", att_request[0].name,"half_day_type")
+		if half_day_type == "Second Half":
+			actual_start_time = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "start_time")), FMT)
+			actual_start_time = actual_start_time.time()
+			actual_et = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "first_half_time")),FMT)
+			actual_end_time = actual_et.time()
+		elif half_day_type == "First Half":
+			actual_st = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "second_half_time")),FMT)
+			actual_start_time = actual_st.time()
+			actual_end_time = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "end_time")),FMT)
+			actual_end_time = actual_end_time.time()
+	elif int(half_day) == 1:
+		actual_start_time = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "start_time")), FMT)
+		actual_start_time = actual_start_time.time()
+		actual_et = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "first_half_time")),FMT)
+		actual_end_time = actual_et.time()
+	else:
+		if not emp[0].shift_type:
+			emp[0].shift_type="General Shift"
+		actual_start_time = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "start_time")),FMT)
+		actual_start_time = actual_start_time.time()
+		actual_end_time = datetime.strptime(str(frappe.db.get_value("Shift Type", emp[0].shift_type, "end_time")),FMT)
+		actual_end_time = actual_end_time.time()
+
+	ct = checkin_type.split(" ")
+
+	if ct[0] == "Office" and ct[1] == "IN":
+		if not emp[0].shift_type:
+			emp[0].shift_type="General Shift"
+		if time.time() > actual_end_time:
+			frappe.throw("Your {shift} has Expired at {actual_end_time} ".format(shift =emp[0].shift_type ,actual_end_time=actual_end_time),title="Cannot Checkin")
+		tdelta = time - datetime.strptime(str(actual_start_time), FMT)
+	elif ct[0] == "Office" and ct[1] == "OUT":
+		tdelta = datetime.strptime(str(actual_end_time), FMT) - time
+	else:
+		tdelta = 0
+
+
+
+	if time.time() > actual_start_time:
+		flag = 1
+		if tdelta != 0:
+			time_difference = (tdelta.seconds/60/60)
+		else:
+			time_difference = 0
+	else:
+		time_difference = 0
+
+	if time.time() < actual_end_time:
+		office_out_flag = 1
+	else:
+		time_difference = 0
+
+	data.append({"employee": emp[0].employee, "employee_name": emp[0].employee_name, "shift_type": emp[0].shift_type, "time":str(time.time()), "time_difference": flt(time_difference,2), "actual_start_time":str(actual_start_time), "flag": flag, "oo_flag":office_out_flag})
+
+	return data if data else data
+@frappe.whitelist()
+def make_employee_checkin(employee, employee_name, shift_type, time, time_difference, reason=None, checkin_type = None):
+	if checkin_type != "":
+		ct = str(checkin_type).split(" ")
+		doc = frappe.new_doc("Employee Checkin")
+		doc.employee = employee
+		doc.emplyoee_name = frappe.db.get_value("Employee", employee, "employee_name")
+		doc.log_type = ct[1]
+		doc.type = ct[0]
+		doc.shift = shift_type
+		doc.date = datetime.strptime(str(nowdate()), "%Y-%m-%d")
+		doc.time_difference = flt(time_difference, 2)
+		if reason != None:
+			doc.reason = reason
+		doc.save(ignore_permissions=True)
+	else:
+		doc = frappe.new_doc("Employee Checkin")
+		doc.employee = employee
+		doc.emplyoee_name = frappe.db.get_value("Employee", employee, "employee_name")
+		doc.log_type = "IN"
+		doc.type = "Office"
+		doc.shift = shift_type
+		doc.date = datetime.strptime(str(nowdate()), "%Y-%m-%d")
+		doc.time_difference = flt(time_difference, 2)
+		if reason != None:
+			doc.reason = reason
+		doc.save(ignore_permissions=True)
 
 @frappe.whitelist()
 def get_energy_points_heatmap_data(user, date):
@@ -26,6 +154,164 @@ def get_energy_points_heatmap_data(user, date):
 		.orderby(Date(eps_log.creation), order=Order.asc)
 		.run()
 	)
+
+
+@frappe.whitelist()
+def get_display_time():
+	d_time = []
+	curr_time = frappe.format_value(now(),{'fieldtype':'Time'})
+	d_time.append({"d_time": curr_time})
+	# frappe.throw(d_time)
+	return d_time
+
+@frappe.whitelist()
+def get_checkin_info(user):
+	data = []
+	office_in = lunch_out = lunch_in = office_out = "00:00:00"
+	cur_date = datetime.strptime(str(nowdate()), "%Y-%m-%d")
+
+	checkin_data = frappe.db.sql("""
+				select ec.type,ec.log_type,date_format(ec.date, '%d-%m-%Y') as att_date,time_format(ec.time, "%H:%i %p") as att_time
+				from `tabEmployee Checkin` ec
+				where ec.date = "{}" and ec.owner = "{}"
+	""".format(cur_date, user), as_dict=True)
+
+	for a in checkin_data:
+		if a.type == "Office" and a.log_type == "IN":
+			office_in = a.att_time
+		elif a.type == "Lunch" and a.log_type == "OUT":
+			lunch_out = a.att_time
+		elif a.type == "Lunch"  and a.log_type == "IN":
+			lunch_in = a.att_time
+		elif a.type == "Office" and a.log_type == "OUT":
+			office_out =a.att_time
+
+	data.append({"office_in": office_in, "lunch_out": lunch_out, "lunch_in": lunch_in, "office_out": office_out, "date": str(cur_date).split(' ')[0]})
+
+	return data
+
+@frappe.whitelist()
+def get_checkin_data(user, month):
+	# frappe.throw(month)
+	default_shift = frappe.db.get_value("Employee",{'user_id':user},"default_shift")
+	in_time = frappe.db.get_value("Shift Type", {'name': default_shift}, "start_time")
+	out_time = frappe.db.get_value("Shift Type", {'name': default_shift}, "end_time")
+	# frappe.throw(str(otime))
+	data = []
+	cur_date = datetime.strptime(str(nowdate()), "%Y-%m-%d")
+	year = str(cur_date).split("-")[0]
+	checkin_list = frappe.db.sql("""
+				select ec.employee, date_format(ec.date, '%d-%m-%Y') as att_date, date_format(ec.date, '%M') as cmonth,
+				date_format(ec.date, '%Y') as cyr
+				from `tabEmployee Checkin` ec
+				where  ec.owner = "{}"
+				and date_format(ec.date, '%M') = "{}"
+				and date_format(ec.date, '%Y') = "{}"
+	""".format(user, month, year), as_dict=True)
+
+	for b in checkin_list:		
+		office_in = lunch_out = lunch_in = office_out = "Not Punch"
+		employee = b.employee
+		month = b.cmonth
+		year = b.cyr
+		checkin_data = frappe.db.sql("""
+				select ec.type,ec.log_type,date_format(ec.date, '%d-%m-%Y') as att_date,time_format(ec.time, "%H:%i %p") as att_time
+				from `tabEmployee Checkin` ec
+				where  ec.employee = "{}"
+				and date_format(ec.date, '%M') = "{}"
+				and date_format(ec.date, '%Y') = "{}"
+				""".format(employee, month, year), as_dict=True)
+		for a in checkin_data:
+			if b.att_date == a.att_date:
+				if a.type == "Office" and a.log_type == "IN":
+					office_in = a.att_time
+				elif a.type == "Lunch" and a.log_type == "OUT":
+					lunch_out = a.att_time
+				elif a.type == "Lunch"  and a.log_type == "IN":
+					lunch_in = a.att_time
+				elif a.type == "Office" and a.log_type == "OUT":
+					office_out = a.att_time
+		data.append({"office_in": office_in, "lunch_out": lunch_out, "lunch_in": lunch_in, "office_out": office_out, "date": b.att_date})
+	
+	new_data = []
+	for a in data:
+		if a not in new_data:
+			new_data.append(a)
+	data = new_data
+	#sorting the data based on date in descending
+	data.sort(key=lambda r: r["date"], reverse=True)
+	
+	return data
+
+@frappe.whitelist()
+def get_employee_checkin_info(user=None):
+	half_day = 0
+	holiday_flag = 0
+	half_day_leave = 0
+	if not user: user = frappe.session.user
+	date = datetime.strptime(str(nowdate()), "%Y-%m-%d")
+	day = calendar.day_name[date.weekday()]
+	# frappe.throw(str(date))
+	if user not in ("Administrator","System Manager"):
+		employee = frappe.db.get_value("Employee",{"user_id": user},"name")
+		holiday_list = get_holiday_list_for_employee(employee)
+
+		leave_details = frappe.db.sql("""
+			select name, half_day from `tabLeave Application` where employee = '{0}'and '{1}' between from_date and to_date and half_day = 1
+			and workflow_state = 'Approved'
+		""".format(employee, date.date()))
+
+		if leave_details:
+			half_day_leave = 1
+		if holiday_list:
+			holidays = frappe.db.sql("""
+					select a.holiday_date from `tabHoliday` a where a.parent = '{0}' and a.holiday_date = '{1}'
+            	""".format(holiday_list, date), as_dict=1)
+
+			if holidays:
+				holiday_flag = 1
+
+			# half_working_days = frappe.db.sql("""
+			# 	select day from `tabHoliday List Days` where parent = '{0}'
+        	# """.format(holiday_list), as_dict=1)
+   
+			flag = 0
+
+			# for a in half_working_days:
+			# 	if day == a.day and flag == 0:
+			# 		half_day = 1
+			# 		flag = 1
+			if half_day == 0:
+				attendance_request = frappe.db.sql("""
+					select name from `tabAttendance Request` where employee = '{0}'
+					and '{1}' >= from_date and '{1}' <= to_date and half_day = 1
+					and docstatus = 1
+                """.format(employee, date))
+				if attendance_request:
+					half_day = 1
+
+	data = frappe.db.sql("""
+				select ec.type,ec.log_type,ec.date as att_date,ec.time as att_time
+				from `tabEmployee Checkin` ec
+				where ec.date = "{}" and ec.owner = "{}" order by creation desc limit 1
+	""".format(date, user), as_dict=True)
+	if data:
+		checkin_type = data[0].type+" "+data[0].log_type
+	else:
+		checkin_type = " "
+
+	return checkin_type, half_day, holiday_flag, half_day_leave
+
+@frappe.whitelist()
+def get_holidays(employee, from_date, to_date, holiday_list = None):
+	'''get holidays between two dates for the given employee'''
+	if not holiday_list:
+		holiday_list = get_holiday_list_for_employee(employee)
+	holidays = frappe.db.sql("""select count(distinct holiday_date) from `tabHoliday` h1, `tabHoliday List` h2
+		where h1.parent = h2.name and h1.holiday_date between %s and %s
+		and h2.name = %s""", (from_date, to_date, holiday_list))[0][0]
+
+	return holidays
 
 
 @frappe.whitelist()
